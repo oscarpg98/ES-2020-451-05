20ff8020c53231b16f4274f22ec3679f690dcf87
paulimartin <65296338+paulimartin@users.noreply.github.com>
2020-05-13 17:31:30 +0200

    Clase "User" atributos hechos

---
+++ User.py
@@ -0,0 +1,9 @@
+class User:
+
+    def __init__(self):
+        self.nombre = str(input())
+        self.dni = int(input())
+        self.direccion = int(input())
+        self.telf = int(input())
+        self.email = str(input())
+        pass
