766d724d8c148ecdffd3c988a097022e6fdce09d
oscarpg98 <38736760+oscarpg98@users.noreply.github.com>
2020-05-13 17:30:55 +0200

    Atributos clase hotels terminados.

---
+++ Hotels.py
@@ -0,0 +1,9 @@
+class Hotels:
+
+    def __init__(self):
+        self.codigo_hotel
+        self.nombre
+        self.n_huespedes
+        self.n_habitaciones
+        self.d_estancia
+        pass
