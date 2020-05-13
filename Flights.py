0223487e438093989f6ce82c651b20caeddffeab
Mariaburrieza <32585751+Mariaburrieza@users.noreply.github.com>
2020-05-13 17:31:46 +0200

    Delete Flights.py

--- Flights.py
+++
@@ -1,8 +0,0 @@
-class Flights:
-
-    def __init__(self):
-        self.id_vuelo
-        self.num_pasajeros = int(input())
-        self.origen = str(input())
-        self.destinacion = str(input())
-        pass
