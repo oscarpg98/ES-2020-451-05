021dfcad2b87ae3602c6af4a04a815ff2267f5db
oscarpg98 <38736760+oscarpg98@users.noreply.github.com>
2020-05-13 17:29:51 +0200

    Delete Hotels.py

--- Hotels.py
+++
@@ -1,9 +0,0 @@
-class Hotels:
-
-    def __init__(self):
-        self.codigo_hotel
-        self.nombre
-        self.n_huespedes
-        self.n_habitaciones
-        self.d_estancia
-        pass
