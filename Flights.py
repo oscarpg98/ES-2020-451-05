0f8186a9a1a87df0d8c56d47bd612f5b174f76ca
Mariaburrieza <32585751+Mariaburrieza@users.noreply.github.com>
2020-05-13 17:31:06 +0200

    Classe "Flights" atributos.

---
+++ Flights.py
@@ -0,0 +1,8 @@
+class Flights:
+
+    def __init__(self):
+        self.id_vuelo
+        self.num_pasajeros = int(input())
+        self.origen = str(input())
+        self.destinacion = str(input())
+        pass
