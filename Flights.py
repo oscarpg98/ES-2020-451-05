27ae827bb6faa2fa627e71976d09dfa615d03198
susana99ssr <57665176+susana99ssr@users.noreply.github.com>
2020-05-17 20:05:11 +0200

    Cambio clase Flights

--- Flights.py
+++ Flights.py
@@ -1,8 +1,16 @@
 class Flights:
 
-    def __init__(self):
-        self.id_vuelo
-        self.num_pasajeros = int(input())
-        self.origen = str(input())
-        self.destinacion = str(input())
+    def __init__(self,codiVol:str, numPasajeros:int, Origen:str, Destinaciones:str):
+        self.id_vuelo=codiVol
+        self.num_pasajeros = numPasajeros
+        self.origen = Origen
+        self.destinaciones = Destinaciones
+        pass
+    
+    def getNumPass(self):
+        return self.num_pasajeros
+        pass
+    
+    def getCodiVol(self):
+        return self.id_vuelo
         pass
