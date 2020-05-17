91fa495057d1e035b9f1f4697ccb3c56538297c4
oscarpg98 <38736760+oscarpg98@users.noreply.github.com>
2020-05-17 20:12:18 +0200

    Gets de Hotels

--- src/Hotels.py
+++ src/Hotels.py
@@ -1,9 +1,24 @@
 class Hotels:
 
-    def __init__(self):
-        self.codigo_hotel
-        self.nombre
-        self.n_huespedes
-        self.n_habitaciones
-        self.d_estancia
+    def __init__(self, codigo_hotel: str, nombre: str, n_huespedes: int, n_habitaciones: int, d_estancia: int):
+        self.codigo_hotel = codigo_hotel
+        self.nombre = nombre
+        self.n_huespedes = n_huespedes
+        self.n_habitaciones = n_habitaciones
+        self.d_estancia = d_estancia
         pass
+
+    def get_codigo_hotel(self):
+        return self.codigo_hotel
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_n_huespedes(self):
+        return self.n_huespedes
+
+    def get_n_habitaciones(self):
+        return self.n_habitaciones
+
+    def get_d_estancia(self):
+        return self.d_estancia
