6355821dac9bd8813ec11bed863f436ce70906e7
Mariaburrieza <32585751+Mariaburrieza@users.noreply.github.com>
2020-05-18 02:42:47 +0200

    Nueva función añadida y corrección de nombres

--- src/Flights.py
+++ src/Flights.py
@@ -1,16 +1,20 @@
 class Flights:
 
-    def __init__(self,codiVol:str, numPasajeros:int, Origen:str, Destinaciones:str):
-        self.id_vuelo=codiVol
-        self.num_pasajeros = numPasajeros
-        self.origen = Origen
-        self.destinaciones = Destinaciones
+    def __init__(self, codi_vol: str = None, num_pasajeros: int = None, origen: str = None, destinacion: str = None):
+        self.id_vuelo = codi_vol
+        self.num_pasajeros = num_pasajeros
+        self.origen = origen
+        self.destinacion = destinacion
         pass
-    
-    def getNumPass(self):
+
+    def get_num_pass(self):
         return self.num_pasajeros
         pass
-    
-    def getCodiVol(self):
+
+    def get_codi_vol(self):
         return self.id_vuelo
+        pass
+
+    def get_destinacion(self):
+        return self.destinacion
         pass
