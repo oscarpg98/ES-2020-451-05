df830fa9246e8f767d0cd40aee074ac4c7a85ba0
oscarpg98 <38736760+oscarpg98@users.noreply.github.com>
2020-05-18 21:02:15 +0200

    Attributes changed and gets done

--- src/User.py
+++ src/User.py
@@ -1,9 +1,23 @@
 class User:
 
-    def __init__(self):
-        self.nombre = str(input())
-        self.dni = int(input())
-        self.direccion = int(input())
-        self.telf = int(input())
-        self.email = str(input())
-        pass
+    def __init__(self, name: str, dni: str, address: str, phone: str, email: str):
+        self.name = name
+        self.dni = dni
+        self.address = address
+        self.phone = phone
+        self.email = email
+
+    def get_name(self):
+        return self.name
+
+    def get_dni(self):
+        return self.dni
+
+    def get_address(self):
+        return self.address
+
+    def get_phone(self):
+        return self.phone
+
+    def get_email(self):
+        return self.email
