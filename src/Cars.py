ebec9ffcb59fcf8678bc425a64a99e2fb698eca2
oscarpg98 <38736760+oscarpg98@users.noreply.github.com>
2020-05-18 20:59:52 +0200

    Attributes changed

--- src/Cars.py
+++ src/Cars.py
@@ -1,8 +1,19 @@
 class Cars:
 
-    def __init__(self):
-        self.codigo
-        self.marca
-        self.lugar_rec
-        self.duracion
-        pass
+    def __init__(self, car_id: str, brand: str, picking_place: str, duration: int):
+        self.car_id = car_id
+        self.brand = brand
+        self.picking_place = picking_place
+        self.duration = duration
+
+    def get_car_id(self):
+        return self.car_id
+
+    def get_brand(self):
+        return self.brand
+
+    def get_picking_place(self):
+        return self.picking_place
+
+    def duration(self):
+        return self.duration
