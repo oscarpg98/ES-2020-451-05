b2c7a7db26e42518adae06f7c050aea1ceee398d
oscarpg98 <38736760+oscarpg98@users.noreply.github.com>
2020-05-18 21:01:45 +0200

    Attributes changed and gets done

--- src/PaymentData.py
+++ src/PaymentData.py
@@ -1,9 +1,24 @@
 class PaymentData:
 
-    def __init__(self):
-        self.tipo = str(input())
-        self.titular = str(input())
-        self.num_tarjeta = int(input())
-        self.cvv = int(input())
-        self.importe = int(input())
+    def __init__(self, card_type: str, owner: str, card_number: str, cvv: int, price: float):
+        self.card_type = card_type
+        self.owner = owner
+        self.card_number = card_number
+        self.cvv = cvv
+        self.price = price
         pass
+
+    def get_card_type(self):
+        return self.card_type
+
+    def get_owner(self):
+        return self.owner
+
+    def get_card_number(self):
+        return self.card_number
+
+    def get_cvv(self):
+        return self.cvv
+
+    def get_price(self):
+        return self.price
