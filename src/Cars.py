00d8a046fa9f79683983a7a4165bee98d357ec11
oscarpg98 <38736760+oscarpg98@users.noreply.github.com>
2020-05-19 02:25:21 +0200

    Price attribute and its get added.

--- src/Cars.py
+++ src/Cars.py
@@ -1,10 +1,11 @@
 class Cars:
 
-    def __init__(self, car_id: str, brand: str, picking_place: str, duration: int):
+    def __init__(self, car_id: str, brand: str, picking_place: str, duration: int, price: float):
         self.car_id = car_id
         self.brand = brand
         self.picking_place = picking_place
         self.duration = duration
+        self.price = price
 
     def get_car_id(self):
         return self.car_id
@@ -15,5 +16,8 @@ class Cars:
     def get_picking_place(self):
         return self.picking_place
 
-    def duration(self):
+    def get_duration(self):
         return self.duration
+
+    def get_price(self):
+        return self.price
