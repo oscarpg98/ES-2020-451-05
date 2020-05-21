85a2aaef8736db0b769b78570c4c3506b3011962
oscarpg98 <38736760+oscarpg98@users.noreply.github.com>
2020-05-21 20:52:41 +0200

    Update TestTrip_v1.py

--- test/Version1/TestTrip_v1.py
+++ test/Version1/TestTrip_v1.py
@@ -1,5 +1,5 @@
 from src.Trip import Trip
-from src.Flights import Flights
+from src.Flight import Flight
 from src.User import User
 from src.PaymentData import PaymentData
 import unittest
@@ -24,22 +24,26 @@ class TestTrip(unittest.TestCase):
 
     def test_add_destination_list(self):
         user = User('Fariseo', '1234567', 'Serranos', '65612648', 'hola@adios.com')
-        flight = Flights('abc', 2, 'Paris', 'Barcelona', 99.99)
+        flight = Flight('abc', 2, 'Paris', 'Barcelona', 99.99)
+        flight2 = Flight('def', 2, 'Barcelona', 'New York', 399.99)
         trip = Trip(user)
         trip.add_flight(flight)
-        self.assertEqual(1, trip.get_flights_len())
+        trip.add_flight(flight2)
+        self.assertEqual(['Barcelona', 'New York'], trip.get_destinations())
 
     def test_add_flight_list(self):
         user = User('Fariseo', '1234567', 'Serranos', '65612648', 'hola@adios.com')
-        flight = Flights('abc', 2, 'Paris', 'Barcelona', 99.99)
+        flight = Flight('abc', 2, 'Paris', 'Barcelona', 99.99)
+        flight2 = Flight('def', 2, 'Barcelona', 'New York', 399.99)
         trip = Trip(user)
         trip.add_flight(flight)
-        self.assertEqual(1, trip.get_destinations_len())
+        trip.add_flight(flight2)
+        self.assertEqual(['abc', 'def'], trip.get_flight_ids())
 
     def test_price_multiple_passengers(self):
         user = User('Fariseo', '1234567', 'Serranos', '65612648', 'hola@adios.com')
-        flight = Flights('abc', 2, 'Paris', 'Barcelona', 99.99)
-        flight2 = Flights('def', 2, 'Barcelona', 'New York', 399.99)
+        flight = Flight('abc', 2, 'Paris', 'Barcelona', 99.99)
+        flight2 = Flight('def', 2, 'Barcelona', 'New York', 399.99)
         trip = Trip(user)
         trip.add_flight(flight)
         trip.add_flight(flight2)
@@ -47,8 +51,8 @@ class TestTrip(unittest.TestCase):
 
     def test_dest_list_deleting_dest(self):
         user = User('Fariseo', '1234567', 'Serranos', '65612648', 'hola@adios.com')
-        flight = Flights('abc', 2, 'Paris', 'Barcelona', 99.99)
-        flight2 = Flights('def', 2, 'Barcelona', 'New York', 399.99)
+        flight = Flight('abc', 2, 'Paris', 'Barcelona', 99.99)
+        flight2 = Flight('def', 2, 'Barcelona', 'New York', 399.99)
         trip = Trip(user)
         trip.add_flight(flight)
         trip.add_flight(flight2)
@@ -57,19 +61,19 @@ class TestTrip(unittest.TestCase):
 
     def test_flight_list_deleting_dest(self):
         user = User('Fariseo', '1234567', 'Serranos', '65612648', 'hola@adios.com')
-        flight = Flights('abc', 2, 'Paris', 'Barcelona', 99.99)
-        flight2 = Flights('def', 2, 'Barcelona', 'New York', 399.99)
+        flight = Flight('abc', 2, 'Paris', 'Barcelona', 99.99)
+        flight2 = Flight('def', 2, 'Barcelona', 'New York', 399.99)
         trip = Trip(user)
         trip.add_flight(flight)
         trip.add_flight(flight2)
         trip.delete_flight(flight)
-        self.assertEqual('def', trip.get_id(flight2))
+        self.assertEqual(['def'], trip.get_flight_ids())
 
     def test_price_deleting_dest(self):
         user = User('Fariseo', '1234567', 'Serranos', '65612648', 'hola@adios.com')
-        flight = Flights('abc', 4, 'Paris', 'Barcelona', 99.99)
-        flight2 = Flights('def', 4, 'Barcelona', 'New York', 399.99)
-        flight3 = Flights('ghi', 4, 'New York', 'Paris', 299.99)
+        flight = Flight('abc', 4, 'Paris', 'Barcelona', 99.99)
+        flight2 = Flight('def', 4, 'Barcelona', 'New York', 399.99)
+        flight3 = Flight('ghi', 4, 'New York', 'Paris', 299.99)
         trip = Trip(user)
         trip.add_flight(flight)
         trip.add_flight(flight2)
@@ -79,34 +83,43 @@ class TestTrip(unittest.TestCase):
 
     def test_payment_done(self):
         user = User('Fariseo', '1234567', 'Serranos', '65612648', 'hola@adios.com')
-        flight = Flights('abc', 4, 'Paris', 'Barcelona', 99.99)
-        flight2 = Flights('def', 4, 'Barcelona', 'New York', 399.99)
-        flight3 = Flights('ghi', 4, 'New York', 'Paris', 299.99)
+        flight = Flight('abc', 4, 'Paris', 'Barcelona', 99.99)
+        flight2 = Flight('def', 4, 'Barcelona', 'New York', 399.99)
+        flight3 = Flight('ghi', 4, 'New York', 'Paris', 299.99)
         trip = Trip(user)
         trip.add_flight(flight)
         trip.add_flight(flight2)
         trip.add_flight(flight3)
         total_price = trip.get_total_price()
         payment = PaymentData('Visa', 'Fariseo', '6456456454', '654', total_price)
-        _, msg = payment.do_payment(user)
-        self.assertEqual('Payment has been done successfully.', msg)
+        answer = trip.do_payment(user, payment)
+        self.assertEqual('Payment has been done successfully.', answer)
 
-    def test_reservation(self):
+    def test_flight_reservation(self):
         user = User('Fariseo', '1234567', 'Serranos', '65612648', 'hola@adios.com')
-        flight = Flights('abc', 4, 'Paris', 'Barcelona', 99.99)
-        flight2 = Flights('def', 4, 'Barcelona', 'New York', 399.99)
-        flight3 = Flights('ghi', 4, 'New York', 'Paris', 299.99)
+        flight = Flight('abc', 4, 'Paris', 'Barcelona', 99.99)
+        flight2 = Flight('def', 4, 'Barcelona', 'New York', 399.99)
+        flight3 = Flight('ghi', 4, 'New York', 'Paris', 299.99)
         trip = Trip(user)
         trip.add_flight(flight)
         trip.add_flight(flight2)
         trip.add_flight(flight3)
-        total_price = trip.get_total_price()
-        payment = PaymentData('Visa', 'Fariseo', '6456456454', '654', total_price)
-        payment.do_payment(user)
-        done, _ = payment.do_payment(user)
-        if done:
-            self.assertEqual(True, trip.confirm_flight_reservation(user))
+        self.assertEqual(True, trip.confirm_flight_reservation(user))
 
 
 if __name__ == '__main__':
     unittest.main()
+
+'''
+@pytest.fixture()
+def trip1():
+    user = User('Fariseo', '1234567', 'Serranos', '65612648', 'hola@adios.com')
+    flight = Flight('abc', 4, 'Paris', 'Barcelona', 99.99)
+    flight2 = Flight('def', 4, 'Barcelona', 'New York', 399.99)
+    flight3 = Flight('ghi', 4, 'New York', 'Paris', 299.99)
+    trip = Trip(user)
+    trip.add_flight(flight)
+    trip.add_flight(flight2)
+    trip.add_flight(flight3)
+    return trip
+'''
