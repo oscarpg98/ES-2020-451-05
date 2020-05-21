d0ffb73c0f723a27e7bd54feccd3c26d37a91508
oscarpg98 <38736760+oscarpg98@users.noreply.github.com>
2020-05-21 20:55:17 +0200

    Update and rename TestFlights_v1.py to TestFlight.py

--- test/Version1/TestFlight.py
+++ test/Version1/TestFlight.py
@@ -1,11 +1,11 @@
-from src.Flights import Flights
+from src.Flight import Flight
 import unittest
 
 
 class TestFlights(unittest.TestCase):
 
     def test_expected_passengers(self):
-        flight = Flights('abc', 2, 'Paris', 'Barcelona', 99.99)
+        flight = Flight('abc', 2, 'Paris', 'Barcelona', 99.99)
         self.assertEqual(2, flight.get_passenger_num())
 
 
